c4d33107b3d0966df19c2727ccaf5a0608f00b88
Alison Lanski <alanski@nd.edu>
2020-12-01 13:57:52 -0500

    adding part 2 of the challenge for day1, still using terrible for loops

--- 2020/2020Day01.R
+++ 2020/2020Day01.R
@@ -5,7 +5,7 @@ library(tidyverse)
 library(xml2)
 library(rvest)
 library(curl)
-thing <- xml2::read_html("https://adventofcode.com/2020/day/1/input")
+xml2::read_html("https://adventofcode.com/2020/day/1/input")
 
 con <- url("https://adventofcode.com/2020/day/1/input.txt", "rb")
 ??read_html
@@ -18,9 +18,10 @@ httr::GET("https://adventofcode.com/2020/day/1/input")
 thing <- html_session("https://adventofcode.com/2020/day/1/input")
 
 #############################
+library(tidyverse)
 
 #this works, but feels like copy-and-paste cheating
-dat <- read_delim("2020/Day1Data.txt", delim = "\n", col_names = FALSE)
+dat <- read_delim("2020/Day01Data.txt", delim = "\n", col_names = FALSE)
 colnames(dat) <- "expense" 
 
 head(dat)
@@ -46,3 +47,21 @@ adding <- function(i, j){
 }
 
 adding(90, 151)
+
+
+#challenge part 2 --- same thing, extra loop really should figure out purrrrrrrr
+for(i in 1:198){
+  for(j in (i+1):199){
+    for(k in (i+2):200){
+      if(dat$expense[k] + dat$expense[i] + dat$expense[j] == 2020){
+        print(paste0(dat$expense[k], "+", dat$expense[i], "+", dat$expense[j]," = ", 
+                     dat$expense[k]+dat$expense[i] +dat$expense[j]))
+        print(dat$expense[k]*dat$expense[i]*dat$expense[j])
+        print(i)
+        print(j)
+        print(k)
+        stop("All done!")
+        }
+      }
+    }
+}
