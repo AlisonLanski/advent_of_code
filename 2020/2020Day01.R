bc46eabe462acb2e8e993ec03dd7db4af6e3fc63
Alison Lanski <alanski@nd.edu>
2020-12-02 08:52:20 -0500

    speeding up processing for day 1 by removing a loop, and adding context

--- 2020/2020Day01.R
+++ 2020/2020Day01.R
@@ -17,6 +17,45 @@ httr::GET("https://adventofcode.com/2020/day/1/input")
 
 thing <- html_session("https://adventofcode.com/2020/day/1/input")
 
+
+# --- Day 1: Report Repair ---
+#   After saving Christmas five years in a row, you've decided to take a vacation at a nice resort on a tropical island. Surely, Christmas will go on without you.
+# 
+# The tropical island has its own currency and is entirely cash-only. The gold coins used there have a little picture of a starfish; the locals just call them stars. None of the currency exchanges seem to have heard of them, but somehow, you'll need to find fifty of these coins by the time you arrive so you can pay the deposit on your room.
+# 
+# To save your vacation, you need to get all fifty stars by December 25th.
+# 
+# Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+#   
+#   Before you leave, the Elves in accounting just need you to fix your expense report (your puzzle input); apparently, something isn't quite adding up.
+# 
+# Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
+# 
+# For example, suppose your expense report contained the following:
+# 
+# 1721
+# 979
+# 366
+# 299
+# 675
+# 1456
+# In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
+# 
+# Of course, your expense report is much larger. Find the two entries that sum to 2020; what do you get if you multiply them together?
+# 
+# Your puzzle answer was 870331.
+# 
+# --- Part Two ---
+# The Elves in accounting are thankful for your help; one of them even offers you a starfish coin they had left over from a past vacation. They offer you a second one if you can find three numbers in your expense report that meet the same criteria.
+# 
+# Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. Multiplying them together produces the answer, 241861950.
+# 
+# In your expense report, what is the product of the three entries that sum to 2020?
+# 
+# Your puzzle answer was 283025088.
+
+
+
 #############################
 library(tidyverse)
 
@@ -65,3 +104,34 @@ for(i in 1:198){
       }
     }
 }
+
+#################################################
+#trying to speed it up 
+#this is MUCH faster for the triple question
+dat2 <- dat
+dat3 <- dat
+
+#part 1 again
+for(i in 1:199){
+    if(2020 %in% (dat$expense[i] + dat2$expense)){
+      k <- which(dat2$expense == 2020-dat$expense[i])
+      print(paste("indices:", i, k))    
+      print(paste("values:", dat$expense[i], dat3$expense[k]))
+      print(paste("product:", dat$expense[i]*dat3$expense[k]))
+      stop("We found it")
+    }
+}
+
+#part 2 again
+for(i in 1:199){
+  for(j in i+1:200){
+    if(2020 %in% (dat$expense[i] + dat2$expense[j] +dat3$expense)){
+      k <- which(dat3$expense == 2020-dat$expense[i]-dat2$expense[j])
+      print(paste("indices:", i, j, k))    
+      print(paste("values:", dat$expense[i], dat2$expense[j], dat3$expense[k]))
+      print(paste("product:", dat$expense[i]*dat2$expense[j]*dat3$expense[k]))
+      stop("We found it")
+    }
+  }
+}
+
